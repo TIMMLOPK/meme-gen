92edcdaf1df10fb85b38bbaded4c9e745fd1c96c
TIMMYLOPK <timmy.12345wer11@gmail.com>
2022-03-06 16:43:21 +0800

    v1.1.2

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import MemeGenerator from "../component/maker"
 
 export default function Component() {
   const { data: session } = useSession()
@@ -7,7 +8,9 @@ export default function Component() {
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        <MemeGenerator />
       </>
+
     )
   }
   return (
