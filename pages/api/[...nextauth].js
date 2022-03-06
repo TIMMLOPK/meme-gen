8bd23a9066cec9e4497675fcf07e2cc46936b970
TIMMYLOPK <timmy.12345wer11@gmail.com>
2022-03-06 15:36:02 +0800

    v1.1

--- pages/api/[...nextauth].js
+++ pages/api/[...nextauth].js
@@ -2,12 +2,12 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
-  ],
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: '694e2a84097f8b56f27c',
+            clientSecret: 'bfafbf91f1d75680991046f34387f7ba8dfeebe3',
+        }),
+        // ...add more providers here
+    ],
 })
